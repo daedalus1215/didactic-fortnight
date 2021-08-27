ef35cbc795837fadfa3d5876f5d91cb0f4286cec
lala <la@la.com>
2021-08-27 16:22:32 -0400

    Add axios and a config file added to .gitignore

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
+const settings = require('./settings.json');
+
+axios.defaults.headers.post['Content-Type'] = 'application/json;charset=utf-8';
+axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
 
 const App: React.FC = () => {
+  React.useEffect(() => {
+    axios.get(settings.url).then(async resp => {
+      const feed = await resp.data;
+      console.log('feed', feed)
+    });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
