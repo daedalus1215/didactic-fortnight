daab114d599fe92a1b996f3b9389eab957379d07
lala <la@la.com>
2021-09-02 22:58:48 -0400

    Add express-xml-bodyparser to server

--- server/src/application/utils/routes.js
+++ server/src/application/utils/routes.js
@@ -3,7 +3,7 @@ const getFeedsAction = require('../actions/getFeedsAction');
 const routes = (app, key) => {
     // const getCryptoPriceRequestHandler = getCryptoPriceAction(key);
     app.get('/status', (req, res) => { res.status(200).jsonp({ success: true }).end(); });
-    app.get('/api/status', (req, res) => { res.status(200).jsonp({ success: true }).end(); });
+    app.get('/api/status', (req, res) => { res.status(201).jsonp({ success: true }).end(); });
     app.get('/api/feeds', getFeedsAction);
 }
 
