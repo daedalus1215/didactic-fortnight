8207025960022484e9da19b77851cdc33346ce44
lala <la@la.com>
2021-09-04 14:14:21 -0400

    Spike in some parsing and create a types

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,51 @@ import React from 'react';
 import logo from './logo.svg';
 import axios from 'axios';
 import './App.css';
+import { IFeed } from './types';
+const XMLParser = require('react-xml-parser');
 const settings = require('./settings.json');
 
 // axios.defaults.headers.get['Content-Type'] = 'application/json;charset=utf-8';
 axios.defaults.headers.get['Access-Control-Allow-Origin'] = '*';
 
 const App: React.FC = () => {
+  const [data, setData] = React.useState({});
+
   React.useEffect(() => {
     axios
-      .get('/api/status', {
+      .get('/api/feeds', {
         headers: {
-          'Content-Type': null,
+          'Content-Type': 'application/xml',
           'Access-Control-Allow-Origin': '*',
         },
       })
-      .then(async resp => {
-        const feed = await resp.data;
-        console.log('feed', feed);
+      .then(resp => {
+        const feed = new window.DOMParser().parseFromString(
+          resp.data,
+          'text/xml'
+        );
+        const items = feed.querySelectorAll('item');
+        const yes: Array<IFeed> = [];
+
+        items.forEach(e => {
+          yes.push({
+            link: e.querySelector('link')?.innerHTML,
+            title: e.querySelector('title')?.innerHTML,
+            author: e.querySelector('author')?.innerHTML,
+            description: e.querySelector('description')?.innerHTML,
+            pubDate: e.querySelector('pubDate')?.innerHTML,
+          });
+        });
+        // const feedItems = { ...items }.map(el => ({
+        //   link: el.querySelector('link').innerHTML,
+        //   title: el.querySelector('title').innerHTML,
+        //   author: el.querySelector('author').innerHTML,
+        // }));
+        console.log('items,', yes);
+
+        // setData(feedItems);
       });
-  }, []);
+  }, [setData]);
 
   return (
     <div className="App">
---
+++ src/types.ts
@@ -0,0 +1,7 @@
+export interface IFeed {
+    link: string|undefined;
+    title: string|undefined;
+    author: string|undefined;
+    description: string|undefined;
+    pubDate: string|undefined;
+}
