4f6c7efcea9fc5fbaa1939f13e6ca3dd2e59516a
lala <la@la.com>
2021-09-11 23:02:00 -0400

    Refactor getFeedsAction

--- server/src/application/actions/getFeedsAction.js
+++ server/src/application/actions/getFeedsAction.js
@@ -1,6 +1,30 @@
+const axios = require('axios');
+const xml2js = require('xml2js');
+const rss = require('../../../settings.json');
 
 const getFeedsAction = (req, response) => {
-    response.jsonp({ 'success': true });
+    // axios.defaults.headers.common = { "Accept": "application/json"};
+    response.header("Content-Type", "application/json");
+
+    const url = rss.filter(r => r.name === req.name).map(r => r.url);
+
+    axios.get(url).then((req, res, next) => {
+        console.log('reg');
+        xml2js.parseString(req.data, async (err, result) => {
+            const items = await result['rss']['channel'][0].item;
+            const data = items.map(item => {
+                return {
+                    title: item.title[0],
+                    description: item.description,
+                    link: item.link,
+                    author: item.author,
+                    pubDate: item.pubDate
+                }
+            });
+            console.log('data', data);
+            response.jsonp({ items: data });
+        })
+    });
 };
 
 module.exports = getFeedsAction;
