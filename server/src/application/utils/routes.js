ec1190522efcf09dcbfbc19cf376826f8667f1c0
lala <la@la.com>
2021-09-11 22:55:38 -0400

    Update routes with action feed list

--- server/src/application/utils/routes.js
+++ server/src/application/utils/routes.js
@@ -1,3 +1,4 @@
+const getFeedListAction = require('../actions/getFeedListAction');
 const getFeedsAction = require('../actions/getFeedsAction');
 
 const routes = (app, key) => {
@@ -5,6 +6,7 @@ const routes = (app, key) => {
     app.get('/status', (req, res) => { res.status(200).jsonp({ success: true }).end(); });
     app.get('/api/status', (req, res) => { res.status(201).jsonp({ success: true }).end(); });
     app.get('/api/feeds', getFeedsAction);
+    app.get('/api/feed-list', getFeedListAction);
 }
 
 module.exports = routes;
