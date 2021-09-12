f737fbdb085c35e84b364967f26e91fe55ae4e75
lala <la@la.com>
2021-09-11 22:57:53 -0400

    Update App, refactor for list view not individual rss

--- src/App.tsx
+++ src/App.tsx
@@ -1,69 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
 import axios from 'axios';
 import './App.css';
 import { IFeed } from './types';
-const XMLParser = require('react-xml-parser');
-const settings = require('./settings.json');
 
 // axios.defaults.headers.get['Content-Type'] = 'application/json;charset=utf-8';
 axios.defaults.headers.get['Access-Control-Allow-Origin'] = '*';
 
 const App: React.FC = () => {
-  const [data, setData] = React.useState({});
-
+  const [data, setData] = React.useState([]);
+  console.log('data', data)
   React.useEffect(() => {
     axios
-      .get('/api/feeds', {
+      .get('/api/feed-list', {
         headers: {
-          'Content-Type': 'application/xml',
+          'Content-Type': 'application/json',
           'Access-Control-Allow-Origin': '*',
         },
       })
       .then(resp => {
-        const feed = new window.DOMParser().parseFromString(
-          resp.data,
-          'text/xml'
-        );
-        const items = feed.querySelectorAll('item');
-        const yes: Array<IFeed> = [];
-
-        items.forEach(e => {
-          yes.push({
-            link: e.querySelector('link')?.innerHTML,
-            title: e.querySelector('title')?.innerHTML,
-            author: e.querySelector('author')?.innerHTML,
-            description: e.querySelector('description')?.innerHTML,
-            pubDate: e.querySelector('pubDate')?.innerHTML,
-          });
-        });
-        // const feedItems = { ...items }.map(el => ({
-        //   link: el.querySelector('link').innerHTML,
-        //   title: el.querySelector('title').innerHTML,
-        //   author: el.querySelector('author').innerHTML,
-        // }));
-        console.log('items,', yes);
-
-        // setData(feedItems);
+        setData(resp.data.items);
       });
   }, [setData]);
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        {data.map((d:any) => {
+          return <li>{d.name}</li>
+        })}
+      </ul>
     </div>
   );
 };
