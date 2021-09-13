cb397a71824c8618753f43ba42b9810b3158a592
lala <13687739+ladams1776@users.noreply.github.com>
2021-09-12 23:43:52 -0400

    Refactor RssPage to have a dynamic fetch

--- src/pages/rss/RssPage.tsx
+++ src/pages/rss/RssPage.tsx
@@ -2,36 +2,44 @@ import React from 'react';
 import axios from 'axios';
 import styles from '../../App.module.css';
 
-const RssPage: React.FC = () => {
-  const [data, setData] = React.useState([]);
+interface IRssPage {
+  code: string;
+}
 
+const RssPage: React.FC<IRssPage> = ({ code }) => {
+  const [data, setData] = React.useState([]);
+  console.log('data', data)
   React.useEffect(() => {
     axios
-      .get('/api/feeds', {
+      .get(`/api/feeds?code=${code}`, {
         headers: {
           'Content-Type': 'application/json',
           'Access-Control-Allow-Origin': '*',
         },
       })
       .then(resp => {
+        console.log('response', resp.data)
         setData(resp.data.items);
       });
-  }, [setData]);
+  }, [code, setData]);
 
   return (
-    <ul>
-      {data?.map((d: any) => {
-        return (
-          <li key={d?.link}>
-            <span className={styles.title}>
-              <a href={d?.link}>{d?.title}</a>
-              {/* <span className={styles.title}>{d?.title}</span> */}
-              <span className={styles.description}>{d?.description}</span>
-            </span>
-          </li>
-        );
-      })}
-    </ul>
+    <>
+      <div>RssPage</div>
+      <ul>
+        {data?.map((d: any) => {
+          return (
+            <li key={d?.link}>
+              <span className={styles.title}>
+                <a href={d?.link}>{d?.title}</a>
+                {/* <span className={styles.title}>{d?.title}</span> */}
+                <span className={styles.description}>{d?.description}</span>
+              </span>
+            </li>
+          );
+        })}
+      </ul>
+    </>
   );
 };
 
