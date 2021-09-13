a495f006cb2382ac67a2adc63cb556db7de1fcfb
lala <13687739+ladams1776@users.noreply.github.com>
2021-09-12 23:43:21 -0400

    Move pieces out of App into Home

---
+++ src/pages/Home.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+interface IHome {
+  data: Array<any>;
+}
+
+const Home: React.FC<IHome> = ({ data }) => {
+  return (
+    <>
+      <div>HomePage</div>
+      <ul>
+        {data?.map((d: any) => {
+          return (
+            <li key={d.code}>
+              <Link to={`/${d.code}`}>{d.name}</Link>
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
+};
+
+export default Home;
