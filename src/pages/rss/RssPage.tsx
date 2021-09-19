3871182a09f28efeca72669d0c43e1889131ef7a
lala <13687739+ladams1776@users.noreply.github.com>
2021-09-18 22:28:16 -0400

    Add RssPage + style

---
+++ src/pages/rss/RssPage.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './RssPage.module.css';
+
+const RssPage = () => {
+    return <div className={styles.rssPage}>yes</div>;
+};
+
+export default RssPage;
