7bb89db8e00a1d8fe1a6452a77651f7b733beaef
lala <13687739+ladams1776@users.noreply.github.com>
2021-09-18 21:43:04 -0400

    Rename Home to ListOfRss and relocate

--- src/pages/listOfRss/ListOfRss.tsx
+++ src/pages/listOfRss/ListOfRss.tsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import cn from 'classnames';
+import styles from './ListOfRss.module.css';
 
 interface RssItem {
   code: string;
-  name: string; 
+  name: string;
 }
 interface IHome {
   feedOptions: Array<RssItem>;
 }
 
-const Home: React.FC<IHome> = ({ feedOptions }) => {
+const ListOfRss: React.FC<IHome> = ({ feedOptions }) => {
+  const [isExpanded, setIsExpanded] = React.useState(true);
+
   return (
-    <>
+    <div className={cn({ 'flex: 9': [isExpanded], 'flex: 0': [!isExpanded] })}>
       <ul>
         {feedOptions?.map((d: any) => {
           return (
@@ -21,8 +25,8 @@ const Home: React.FC<IHome> = ({ feedOptions }) => {
           );
         })}
       </ul>
-    </>
+    </div>
   );
 };
 
-export default Home;
+export default ListOfRss;
