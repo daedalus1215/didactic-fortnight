589a9fecbe2bdafd3ad6d35a51828a0b7f5b4f96
lala <13687739+ladams1776@users.noreply.github.com>
2021-09-23 22:12:20 -0400

    Add more styles to Main

--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { match } from 'react-router-dom';
 import { IFeedTemplate } from '../../types';
-import ListOfRss from './listOfRss/ListOfRss';
+import ListOfRss from './listOfRss/RssTemplateListView';
 import RssPage from './rss/RssPage';
 import styles from './Main.module.css';
+import Navbar from './navbar/Navbar';
 interface IMain {
   feedOptions: Array<IFeedTemplate>;
   match: {
@@ -14,11 +15,23 @@ interface IMain {
 }
 
 const Main: React.FC<IMain> = ({ feedOptions, match }) => {
+  const [isExpanded, setIsExpanded] = useState(false);
   const code = match?.params?.code || 'unknown';
+
   return (
     <div className={styles.main}>
-      <ListOfRss feedOptions={feedOptions} />
-      <RssPage feedOptions={feedOptions} code={code} />
+      <Navbar isExpanded={isExpanded} setIsExpanded={setIsExpanded} />
+      <div className={styles.body}>
+        <ListOfRss
+          feedOptions={feedOptions}
+          isExpanded={isExpanded}
+        />
+        <RssPage
+          feedOptions={feedOptions}
+          code={code}
+          isExpanded={isExpanded}
+        />
+      </div>
     </div>
   );
 };
