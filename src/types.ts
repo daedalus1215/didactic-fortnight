78d5a45934aae2dfc1fbb36371b1f0821de12c77
lala <13687739+ladams1776@users.noreply.github.com>
2021-09-25 21:33:53 -0400

    Add more to types

--- src/types.ts
+++ src/types.ts
@@ -4,7 +4,7 @@ export interface IFeed {
     author: string|undefined;
     description: string|undefined;
     pubDate: string|undefined;
-}
+};
 
 export interface IFeedTemplate {
     url: string;
@@ -12,15 +12,27 @@ export interface IFeedTemplate {
     id: number;
     code: string;
     rssTemplate: string;
-}
+};
 
 export interface IRssPageByCode {
     code: string;
     isExpanded: boolean;
-}
+    feedOptions: Array<IFeedTemplate>;
+};
 
 export interface IRssPage {
     feedOptions: Array<IFeedTemplate>;
     code: string;
     isExpanded: boolean;
-}
+};
+
+export interface IRssItem {
+    rss: {
+        link: string;
+        image: string;
+        text: string;
+        title: string;
+        description: string;
+        pubDate: string;
+    }
+};
