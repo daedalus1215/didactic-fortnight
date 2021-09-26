4abc8551c3ac05fd5eef2d3e2e56d2cbdaaa7a33
lala <13687739+ladams1776@users.noreply.github.com>
2021-09-25 21:34:12 -0400

    Restructure RssPage no need for conditional

--- src/pages/main/rss/RssPage.tsx
+++ src/pages/main/rss/RssPage.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { IFeedTemplate, IRssPage } from '../../../types';
 import RssPageByCode from '../rss/RssPageByCode';
-import RssPageByCodeRedit from '../rss/RssPageByCodeRedit';
 import styles from './RssPage.module.css';
 
 const RssPage: React.FC<IRssPage> = ({ feedOptions, code, isExpanded }) => {
@@ -10,26 +9,15 @@ const RssPage: React.FC<IRssPage> = ({ feedOptions, code, isExpanded }) => {
       {feedOptions
         .filter(o => o.code === code)
         .map((feedTemplate: IFeedTemplate) => {
-          if (feedTemplate.rssTemplate === 'generic') {
-            return (
-              <RssPageByCode
-                code={feedTemplate.code}
-                key={feedTemplate.id}
-                isExpanded={isExpanded}
-              />
-            );
-          } else if (feedTemplate.rssTemplate === 'redit') {
-            return (
-              <RssPageByCodeRedit
-                code={feedTemplate.code}
-                key={feedTemplate.id}
-                isExpanded={isExpanded}
-              />
-            );
-          } else {
-            return [];
-          }
-        }) || []}
+          return (
+            <RssPageByCode
+              code={feedTemplate.code}
+              key={feedTemplate.id}
+              isExpanded={isExpanded}
+              feedOptions={feedOptions}
+            />
+          );
+        })}
     </>
   );
 };
