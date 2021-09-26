9b8064d510e6cebb6411da7dd1d1af7e198f39db
lala <13687739+ladams1776@users.noreply.github.com>
2021-09-25 21:35:58 -0400

    Restructure RssPageByCode So that it. picks the items based on the template

--- src/pages/main/rss/RssPageByCode.tsx
+++ src/pages/main/rss/RssPageByCode.tsx
@@ -2,41 +2,62 @@ import React from 'react';
 import cn from 'classnames';
 import axios from 'axios';
 import { IRssPageByCode } from '../../../types';
+import RssItem from './rssItem/RssItem';
 import styles from './RssPageByCode.module.css';
+import ReditItem from './reditItem/ReditItem';
 
-const RssPageByCode: React.FC<IRssPageByCode> = ({ code, isExpanded }) => {
+const RssPageByCode: React.FC<IRssPageByCode> = ({
+  code,
+  isExpanded,
+  feedOptions,
+}) => {
   const [data, setData] = React.useState([]);
 
-  React.useEffect(() => {
-    axios
-      .get(`/api/feeds?code=${code}`, {
-        headers: {
-          'Content-Type': 'application/json',
-          'Access-Control-Allow-Origin': '*',
-        },
-      })
-      .then(resp => {
-        // console.log('response', resp.data);
-        setData(resp.data.items);
-      });
-  }, [code, setData]);
+  const rssTemplate = feedOptions
+    .filter(f => f.code === code)
+    .map(f => f.rssTemplate)[0];
 
+  React.useEffect(() => {
+    if (rssTemplate === 'redit') {
+      axios
+        .get(`/api/redit-feeds?code=${code}`, {
+          headers: {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*',
+          },
+        })
+        .then(resp => {
+          setData(resp.data.items);
+        });
+    } else if (rssTemplate === 'generic') {
+      axios
+        .get(`/api/feeds?code=${code}`, {
+          headers: {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*',
+          },
+        })
+        .then(resp => {
+          setData(resp.data.items);
+        });
+    }
+  }, [code, rssTemplate, setData]);
+
+  console.log('code', rssTemplate);
+  console.log('data', data);
   return (
     <div className={cn(styles.listWrapper, { 'flex 1': isExpanded })}>
       <ul className={styles.ul}>
-        {data?.map((d: any) => {
-          return (
-            <li key={d?.link} className={styles.li}>
-              <a href={d?.link}>
-                <img src={d.image} alt="" className={styles.img} />
-                <div className={styles.text}>
-                  <div className={styles.title}>{d?.title}</div>
-                  <div className={styles.description}>{d?.description}</div>
-                  <div className={styles.pubDate}>{d.pubDate}</div>
-                </div>
-              </a>
-            </li>
-          );
+        {data?.map((rss: any) => {
+          if (rssTemplate === 'generic') {
+            console.log('yes', rss);
+            return <RssItem rss={rss} key={rss.link} />;
+          } else if (rssTemplate === 'redit') {
+            console.log('rssTemplte', 'ReditItem');
+            return <ReditItem rss={rss} key={rss.link} />;
+          } else {
+            return [];
+          }
         })}
       </ul>
     </div>
